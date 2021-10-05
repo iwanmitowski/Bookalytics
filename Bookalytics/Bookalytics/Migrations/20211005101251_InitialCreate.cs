617b938bd0f2e1faeb7efd1336ce528dfe3206c2
iwanmitowski <iwan.mitowski@gmail.com>
2021-10-05 15:18:11 +0300

    Adding Migration. Adding BookAnalyzerService and BookScrapperService

---
+++ Bookalytics/Bookalytics/Migrations/20211005101251_InitialCreate.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bookalytics.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    WordsCount = table.Column<int>(type: "int", nullable: false),
+                    ShortestWord = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LongestWord = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MostCommonWord = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MostCommonWordCount = table.Column<int>(type: "int", nullable: false),
+                    LeastCommonWord = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LeastCommonWordCount = table.Column<int>(type: "int", nullable: false),
+                    AverageWordLength = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
---
+++ Bookalytics/Bookalytics/Services/Contracts/IBookScrapperService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookalytics.Services.Contracts
+{
+    public interface IBookScrapperService
+    {
+    }
+}
