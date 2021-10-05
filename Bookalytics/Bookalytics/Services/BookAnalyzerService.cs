36ccd5ba950ed139b24a77b9aca9fc83990a0d55
iwanmitowski <iwan.mitowski@gmail.com>
2021-10-05 15:22:48 +0300

    Uploading the project with only the necessary files

---
+++ Bookalytics/Bookalytics/Services/BookAnalyzerService.cs
@@ -0,0 +1,89 @@
+﻿using Bookalytics.Services.Contracts;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookalytics.Services
+{
+    public class BookAnalyzerService : IBookAnalyzerService
+    {
+        private readonly List<string> words;
+        private readonly ConcurrentDictionary<string, int> wordsAppearances;
+        public BookAnalyzerService(string bookText)
+        {
+            this.BookText = bookText;
+            wordsAppearances = new ConcurrentDictionary<string, int>();
+            words = GetWords();
+            WordsAppearances();
+        }
+        public ConcurrentDictionary<string, int> GetDict() => this.wordsAppearances;
+
+        public string BookText { get; set; }
+
+        public double AverageWordLength()
+        {
+            return words.Select(x => x.Length).Average();
+        }
+
+        //Alphabetically
+        public string GetLeastCommonWord()
+        {
+            var word = this.wordsAppearances.OrderBy(x => x.Value).ThenBy(x => x.Key).FirstOrDefault().Key;
+
+            return word;
+        }
+
+        public int GetLeastCommonWordCount(string word)
+        {
+            var count = wordsAppearances[word];
+
+            return count;
+        }
+
+        public string GetMostCommonWord()
+        {
+            var word = this.wordsAppearances.OrderByDescending(x => x.Value).ThenBy(x => x.Key).FirstOrDefault().Key;
+
+            return word;
+        }
+
+        public int GetMostCommonWordCount(string word)
+        {
+            var count = wordsAppearances[word];
+
+            return count;
+        }
+
+        public string GetLongestWord()
+        {
+            var word = this.wordsAppearances.Keys.OrderByDescending(x => x.Length).ThenBy(x => x).FirstOrDefault();
+
+            return word;
+        }
+        public string GetShortestWord()
+        {
+            var word = this.wordsAppearances.Keys.OrderBy(x => x.Length).ThenBy(x=>x).FirstOrDefault();
+
+            return word;
+        }
+
+        public int GetWordsCount()
+        {
+            var count = words.Count();
+
+            return count;
+        }
+
+        private List<string> GetWords() => BookSerializer.ReturnWordsToUpper(this.BookText);
+
+        private void WordsAppearances()
+        {
+            Parallel.ForEach(words, (currentWord) =>
+            {
+                wordsAppearances.AddOrUpdate(currentWord, 1, (string x, int y) => y + 1);
+            });
+        }
+    }
+}
---
+++ Bookalytics/Bookalytics/Services/Contracts/IBookAnalyzerService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookalytics.Services.Contracts
+{
+    public interface IBookAnalyzerService
+    {
+        public IDictionary<string, int> GetDict();
+        public int GetWordsCount();
+        public string GetShortestWord();
+        public string GetLongestWord();
+        public string GetMostCommonWord();
+        public int GetMostCommonWordCount(string word);
+        public string GetLeastCommonWord();
+        public int GetLeastCommonWordCount(string word);
+        public double AverageWordLength();
+    }
+}
